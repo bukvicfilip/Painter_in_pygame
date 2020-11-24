6c65cf3b04861e11b8e4b13f80f48c6f48daed2d
Filip Bukvić <bukvifilip6@gmail.com>
2020-11-25 00:17:33 +0100

    Correcting the class methods

--- program.py
+++ program.py
@@ -10,7 +10,9 @@ x_rect=380
 clr=[255, 255, 255]
 pressed=[False, False, False]
 base_font=pygame.font.Font(None, 15)
+base_font2=pygame.font.Font(None, 11)
 
+#Colors from the palette
 colors=[[0, 80, 239],[27, 161, 226], [0, 171, 169],
 		[100, 118, 135], [109, 135, 100], [118, 96, 138],
 		[229, 20, 0], [162, 0, 37], [216, 0, 115],
@@ -27,28 +29,25 @@ n=0
 c=0
 a=0
 
+#Toolbar class
 class Menu:
 	def __init__(self, color, position, clr):
 		self.screen=screen
 		self.toolbar = pygame.draw.rect(self.screen, color, position, border_radius=50)
-		self.central = pygame.draw.rect(screen, clr, (x_rect, 8, 40, 40))
+		self.central = pygame.draw.rect(screen, clr, (x_rect, 5, 40, 30))
 
-	def coordinates(x1, x2, x0, x02, n):
-		for num in range(6):
-			x1.append(x0)
-			x2.append(x02)
-			x0+=25
-			x02-=25
-
-	def draw_palette(c):
+	#Draws a palette on the toolbar
+	def draw_palette(self, c):
 		for n in range(len(x1)):
-			pygame.draw.rect(screen, colors[c], (x1[n], 5, 20, 20))
-			pygame.draw.rect(screen, colors[c+6], (x2[n], 5, 20, 20))
-			pygame.draw.rect(screen, colors[c+18], (x1[n], 30, 20, 20))
-			pygame.draw.rect(screen, colors[c+12], (x2[n], 30, 20, 20))
+			pygame.draw.rect(self.screen, colors[c], (x1[n], 5, 20, 20))
+			pygame.draw.rect(self.screen, colors[c+6], (x2[n], 5, 20, 20))
+			pygame.draw.rect(self.screen, colors[c+18], (x1[n], 30, 20, 20))
+			pygame.draw.rect(self.screen, colors[c+12], (x2[n], 30, 20, 20))
 			c+=1
 			n+=1
-	def draw_circle(x_pos, y_pos, pressed):
+
+	#Draws buttons on the toolbar
+	def draw_circle(self, x_pos, y_pos, pressed):
 		x=595
 		y=14
 		color=(208,202, 230)
@@ -73,16 +72,25 @@ class Menu:
 		screen.blit(letter_b, (732, 9))
 		screen.blit(letter_g, (757, 9))
 
-	def draw_undo_and_x():
+	#Draws undo, remove and save button on the toolbar
+	def draw_undo_and_x(self):
 		screen.blit(undo_button, (620, 11))
 		screen.blit(x_button, (660, 11))
+		screen.blit(save_button, (30, 11))
 
-	def rgb_text(text):
+	#Draws text of rgb code
+	def rgb_text(self, text):
 		n=0
 		for num, code in enumerate(text):
 			text_surface=base_font.render(str(round(text[num])), True, (0, 0, 0))
 			screen.blit(text_surface, (707+n, 35))
 			n+=25
+
+	def tap_to_save(self):
+		text_surface2=base_font2.render("CLICK TO SAVE", True, (0, 0, 0))
+		text_surface3=base_font2.render("THE COLOR", True, (0, 0, 0))
+		screen.blit(text_surface2, (374, 37))
+		screen.blit(text_surface3, (380, 44))
 
 
 # Screen creating
@@ -90,12 +98,20 @@ screen = pygame.display.set_mode((width, height),
 	flags = pygame.RESIZABLE)
 screen.fill((255, 255, 255))
 
-Menu.coordinates(x1, x2, x0, x02, n)
-
+#Filling in pixels
 def pixel(surface, pos, color):
     surface.fill(color, (pos, (5, 5)))
 
-#Title and Icon
+def coordinates(x1, x2, x0, x02, n):
+	for num in range(6):
+		x1.append(x0)
+		x2.append(x02)
+		x0+=25
+		x02-=25
+
+coordinates(x1, x2, x0, x02, n)
+
+#Title, pictures and Icon
 pygame.display.set_caption("Painter")
 icon = pygame.image.load("paintbrush.png")
 pygame.display.set_icon(icon)
@@ -106,6 +122,7 @@ minus_button=pygame.image.load("minus.png")
 letter_r=pygame.image.load("r.png")
 letter_g=pygame.image.load("b.png")
 letter_b=pygame.image.load("g.png")
+save_button=pygame.image.load("download.png")
 
 pygame.mouse.set_cursor(*pygame.cursors.diamond)
 
@@ -128,9 +145,13 @@ while running:
 				pressed[2]=True
 			elif 753<x<802 and 5<y<29 and pygame.mouse.get_pressed()[0]==True and pressed[2]==True:
 				pressed[2]=False
+			if 410>x>380 and 48>y>8:
+				colors.pop(1)
+				colors.append(clr)
 
 	x, y = pygame.mouse.get_pos()
 
+	#Checks when we press right button with mouse
 	if pygame.mouse.get_pressed()[0]:
 		pixel(screen, (x, y), clr)
 
@@ -179,14 +200,22 @@ while running:
 			else:
 				pass
 
+		#Logic when you press "X" button
 		if 695>x>660 and 40>y>11:
 			screen.fill((255, 255, 255))
 
-	Menu([227,223, 245], (0, 0, width, 55), clr)
-	Menu.draw_palette(c)
-	Menu.draw_circle(x, y, pressed)
-	Menu.draw_undo_and_x()
-	Menu.rgb_text(clr)
+		#Logic when you press "Save" button
+		if 62>x>30 and 43>y>11:
+			rect = pygame.Rect(0, 55, 800, 545)
+			sub = screen.subsurface(rect)
+			pygame.image.save(sub, "screenshot.png")
+
+	paint=Menu([227,223, 245], (0, 0, width, 55), clr)
+	paint.tap_to_save()
+	paint.draw_palette(c)
+	paint.draw_circle(x, y, pressed)
+	paint.draw_undo_and_x()
+	paint.rgb_text(clr)
 	pygame.display.update()
 	clock.tick(60)
 pygame.quit()
